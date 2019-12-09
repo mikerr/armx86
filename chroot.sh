c8727018c98636d0902ac74708902a5c0877d626
Mike Redrobe <mike@redrobe.net>
2019-12-09 10:26:05 +0000

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,1 +1,7 @@
+sudo mount -t sysfs sys /tmp/mnt/sys/
+sudo mount -t proc proc /tmp/mnt/proc/
+sudo mount –bind /dev /tmp/mnt/dev/
+sudo mount –bind /dev/pts /tmp/mnt/dev/pts/
+sudo mount –bind /dev/shm /tmp/mnt/dev/shm/
+
 sudo chroot /home/pi/armx86/x86 /bin/su -l root
